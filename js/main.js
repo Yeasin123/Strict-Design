b5e42e496b5bf72e396feff1e229753f1f3a77ed
Yeasin123 <yeasin15-1037@diu.edu.bd>
2020-05-09 14:05:24 +0600

    Strict Web Design

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$('.owl-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            autoplay:true,
+            autoplayTimeout:6000, 
+            nav:false,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:1
+                },
+                1000:{
+                    items:4
+                }
+            }
+});
+
+ jQuery(function($) {
+      $(document).ready( function() {
+        //enabling stickUp on the '.navbar-wrapper' class
+        $('.navbar-wrapper').stickUp();
+     });
+ });
+
+
+
+
+//Initialize material-scrolltop (minimal)
+
+  $('body').materialScrollTop();
+
+
+
+
+
+
+
+
